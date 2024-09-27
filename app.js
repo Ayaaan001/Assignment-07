7ba21e872fd65d6160057b3f6027f64301af62d1
Ayaaan001 <159950674+Ayaaan001@users.noreply.github.com>
2024-09-27 11:17:06 +0500

    add

---
+++ app.js
@@ -0,0 +1,9 @@
+var userInput = prompt("Enter a palindrome:");
+
+var reversedStr = userInput.split('').reverse().join('');
+
+if (userInput === reversedStr) {
+    console.log('It is a palindrome');
+} else {
+    console.log('It is not a palindrome');
+}
